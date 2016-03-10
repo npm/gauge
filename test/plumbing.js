b2ae02d856b9219e5675ba17b1281f98f6aee4c8
Rebecca Turner <me@re-becca.org>
2016-03-09 16:02:01 -0800

    Make the plumbing tests fully unit tests

--- test/plumbing.js
+++ test/plumbing.js
@@ -1,35 +1,42 @@
 'use strict'
 var test = require('tap').test
-var stripAnsi = require('strip-ansi')
-var Plumbing = require('../plumbing.js')
-var themes = require('../themes.js')
-
-var stripInvisible = function (str) {
-  return stripAnsi(str).replace(/^\s+|\s+$/mg, '')
-}
+var requireInject = require('require-inject')
+var Plumbing = requireInject('../plumbing.js', {
+  '../render-template.js': function (width, template, values) {
+    if (values.x) values.x = values.x // pull in from parent object for stringify
+    return 'w:' + width + ', t:' + JSON.stringify(template) + ', v:' + JSON.stringify(values)
+  },
+  '../console-strings.js': {
+    eraseLine: function () { return 'ERASE' },
+    gotoSOL: function () { return 'CR' },
+    hideCursor: function () { return 'HIDE' },
+    showCursor: function () { return 'SHOW' }
+  }
+})
 
 var template = [
   {type: 'name'}
 ]
-var plumbing = new Plumbing(themes.getTheme('ASCII'), template, 10)
+var theme = {}
+var plumbing = new Plumbing(theme, template, 10)
 
 // These three produce fixed strings and are entirely static, so as long as
 // they produce _something_ they're probably ok. Actually testing them will
 // require something that understands ansi codes.
 test('showCursor', function (t) {
-  t.ok(plumbing.showCursor())
+  t.is(plumbing.showCursor(), 'SHOW')
   t.end()
 })
 test('hideCursor', function (t) {
-  t.ok(plumbing.hideCursor())
+  t.is(plumbing.hideCursor(), 'HIDE')
   t.end()
 })
 test('hide', function (t) {
-  t.ok(plumbing.hide())
+  t.is(plumbing.hide(), 'CRERASE')
   t.end()
 })
 
 test('show', function (t) {
-  t.is(stripInvisible(plumbing.show({name: 'test'})), 'test')
+  t.is(plumbing.show({name: 'test'}), 'w:10, t:[{"type":"name"}], v:{"name":"test"}ERASECR')
   t.end()
 })
